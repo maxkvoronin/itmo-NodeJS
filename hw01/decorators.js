5b14d3dfe6288d3a8b22d77727fd760bf888f4e8
maxim <maxkvoronin@gmail.com>
2018-06-06 21:07:59 +0300

    hw01 1,2* refactor

--- hw01/decorators.js
+++ hw01/decorators.js
@@ -1,71 +1,67 @@
-/**
+/*
  * 1. Требуется реализовать декоратор с параметрами pause, 
  * который откладывает выполнение функции на указанное 
  * количество секунд. Пример использования:
  * function func() {
- *       console.log('Функция выполниться с задержкой в 2 секунды!');
+ *	 console.log('Функция выполниться с задержкой в 2 секунды!');
  * }
- *  pause(func, 2);
- *
-*/
+ * let paused = pause(func, 2);
+ * paused();
+ */
 
-function func(...args) {
-    console.log(`Функция выполниться с задержкой в ${args[1]} секунды!`);
+function func() {
+	console.log(`Функция выполниться с задержкой в ${arguments[1]} секунды!`);
 }
+let paused = pause(func, 2);
+paused();
 
-function decorator(func){
-    function wrapper(...args) {
-        setTimeout(()=>{return func(...args)},args[1]*1000); 
-    } 
-    return wrapper;
+function pause(func) {
+    	return () => {
+    		setTimeout(func, arguments[1]*1000, ...arguments); 
+    	} 
 }
-
-var pause = decorator(func);
-pause(func,2);
 
 /*
- *  2*. Требуется реализовать декоратор с параметрами returnObject, 
+ * 2*. Требуется реализовать декоратор с параметрами returnObject, 
  * который в случае, если функция возвращает массив, подменяет 
  * его объектом. Имена задаются в параметрах декоратора. Декоратор 
  * универсальный, количество имен переменное.
  * Пример использования №1:
  * function func(){
- *   return [1, 2]
+ * 	return [1, 2]
  * }
- * r = return_object (func, 'one', 'two');
+ * let func_decoreted = return_object(func, 'one', 'two');
+ * let r = func_decoreted();
  * console.log(r.one); // 1
  * console.log(r.two); //2
  * Пример использования №2:
- *  function func(){
- *   return ['Python', 'is', 'programming language']
+ * function func(){
+ * 	return ['Python', 'is', 'programming language']
  * }
- * r = return_object (func, 'a', 'b', 'c');
+ * r = return_object (func, 'a', 'b', 'c')();
  * console.log(r.c) // 'programming language'
- */
+ * */
 
 function func1(){
-        return ['Python', 'is', 'programming language'];
+	return [1, 2]
 }
+let func_decoreted = return_object(func1, 'one', 'two');
 
-var r = return_object (func1, 'a', 'b', 'c');
-console.log(r.c); // 1
+let r = func_decoreted();
+console.log(r.one); // 1
+console.log(r.two); //2
 
 function return_object(func, ...args){
-    function wrapper(args) {
-        var temp = func(...args);   
+	return () => {
+		let arr = func();   
 
-        if (Array.isArray(temp)) {
-            var obj = {};
-            args.forEach((elem, i)=>{
-                obj[elem] = temp[i];
-            })
+        if (Array.isArray(arr)) {
+			let obj = {};
+            for(let i=0;i<=arr.length;i++) {
+				obj[args[i]] = arr[i];
+            }
             return obj;
-        }
-
-        return temp;
+		}
+        return arr;
     }
-    return wrapper(args);
 }
-
-
-
