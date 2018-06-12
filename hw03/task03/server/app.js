4da543d9906e8570dc6350a174a664d04ad1f5fd
maxim <maxkvoronin@gmail.com>
2018-06-12 23:32:30 +0300

    HomeWork task 1,2,3 done

--- hw03/task03/server/app.js
+++ hw03/task03/server/app.js
@@ -6,7 +6,7 @@ let server = http.createServer().listen(8080);
 server.on('request', (req, res) => {
   res.writeHead(200, {'Content-Type' : 'application/json',
                       'Access-Control-Allow-Origin': '*'
-  })
+  });
   
   res.end(JSON.stringify(json[Math.floor(Math.random()*json.length)]));
-})
+});
