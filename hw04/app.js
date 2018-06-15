c9bad8c4cd6b8463cdcd720d2a52b61c9fa75674
maxim <maxkvoronin@gmail.com>
2018-06-15 16:49:56 +0300

    homework04 done

---
+++ hw04/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs'); 
+const fileName = "index.html";
+
+let parser = require('./parser');
+let i = 0;
+
+let server = http.createServer((req, res) => {
+  fs.readFile(fileName, 'utf8', function(err, data) {
+      if (err)
+          console.log('Не могу открыть файл' );
+      else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data);
+      }
+  })
+}).listen(8080);
+
+
+server.on('request', (req, res) => {
+  req.on('data', function(data) {
+    parser(data.toString().split('\n'), json => {
+      fs.writeFile(`result${++i}.json`, json, err => {
+        if (err) return console.log(err);
+        else console.log(`Сервак распарсил входящий файл и записал его в result${i}.json`);
+      });
+    });
+  });
+});
+
---
+++ hw04/parser.js
@@ -0,0 +1,28 @@
+module.exports = (arr, callback) => {
+  let json = {};
+
+  arr.forEach((row, i) => {
+    row = row.replace(/\r/,'');
+    
+    if (row.includes('GET')) {
+      let header = row.split(' ');
+        json['method'] = header[0];
+        json['uri'] = header[1];
+        json['protocol'] = header[2];
+    } 
+
+    else if (row.includes('HTTP/')) {
+      let header = row.split(' ');
+        json['protocol'] = header[0];
+        json['status_code'] = header[1];
+        json['status_message'] = header.splice(2).join(' ');
+    }
+
+    else if (i >= 4 && i < arr.length - 4) {
+        let nw = row.split(': ');
+        json[nw[0]] = nw[1];
+    }
+
+  });
+  return callback(JSON.stringify(json, null, '  '));
+}
